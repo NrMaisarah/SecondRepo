0ad846a1dc47019fcc82f47ba8884033776400c6
NrMaisarah <2022470866@student.uitm.edu.my>
2024-07-10 14:26:57 +0800

    add controller

---
+++ src/main/java/com/heroku/controller/PetfoodController.java
@@ -0,0 +1,44 @@
+package com.heroku.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.heroku.java.models.Petfood;
+import com.heroku.services.PetfoodService;
+
+@Controller
+@RequestMapping("/petfood")
+public class PetfoodController {
+
+    @Autowired
+    private PetfoodService PetfoodService;
+
+    @GetMapping("/create")
+    public String showCreateForm(@RequestParam("staffID") int staffID, Model model) {
+        model.addAttribute("staffID", staffID);
+        return "createPetFood";
+    }
+
+    @PostMapping("/create")
+    public String createPetFood(@ModelAttribute Petfood petfood, RedirectAttributes redirectAttributes) {
+        try {
+            PetfoodService.createPetFood(petfood);
+            redirectAttributes.addFlashAttribute("message", "Pet food created successfully!");
+        } catch (NumberFormatException e) {
+            redirectAttributes.addFlashAttribute("error", "Invalid input: Please enter valid numbers for price, weight, quantity, and reorder point.");
+            return "redirect:/petfood/create?staffID=" + petfood.getStaffID();
+        }
+        
+        return "redirect:/petfood/list?staffID=" + petfood.getStaffID();
+    }
+
+    @GetMapping("/list")
+    public String listPetFood(@RequestParam("staffID") int staffID, Model model) {
+        model.addAttribute("petfoods", PetfoodService.getAllPetFood());
+        model.addAttribute("staffID", staffID);
+        return "listPetFood";
+    }
+}
---
+++ src/main/java/com/heroku/services/PetfoodService.java
@@ -0,0 +1,40 @@
+package com.heroku.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.heroku.java.models.Petfood;
+import com.heroku.repositories.PetfoodRepository;
+
+import java.util.List;
+
+@Service
+public class PetfoodService {
+
+    @Autowired
+    private PetfoodRepository petfoodRepository;
+
+    @Transactional
+    public void createPetFood(Petfood petfood) {
+        petfoodRepository.save(petfood);
+    }
+
+    public Petfood getPetFoodById(int inventoryID) {
+        return petfoodRepository.getPetFoodById(inventoryID);
+    }
+
+    public List<Petfood> getAllPetFoods() {
+        return petfoodRepository.getAllPetFoods();
+    }
+
+    @Transactional
+    public void updatePetFood(Petfood petfood) {
+        petfoodRepository.save(petfood);
+    }
+
+    @Transactional
+    public void deletePetFood(int inventoryID) {
+        petfoodRepository.deleteById(inventoryID);
+    }
+}
